2da6804a69773f7c8ae13efa9288c416181c2d3e
mhf <1603291350@qq.com>
2022-03-27 15:38:09 +0800

    新功能：添加串口、鼠标键盘热插拔监测模块
        1、添加功能模块；
        2、增加Windows系统宏判断功能；
        3、在用到windows.h功能的地方引入user32.lib库，防止报错；
        4、修改README.md文件，增加设备管理模块说明内容。

---
+++ DeviceManagement/DeviceManagement.pro
@@ -0,0 +1,48 @@
+#---------------------------------------------------------
+# 功能：       windows下串口、鼠标、键盘热插拔检测模块热插拔监测demo
+# 编译器：
+#
+# @开发者     mhf
+# @邮箱       1603291350@qq.com
+# @时间       2022/03/27
+# @备注
+#---------------------------------------------------------
+QT       += core gui serialport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    comchange.cpp \
+    main.cpp \
+    mousekeytest.cpp \
+    porttest.cpp \
+    widget.cpp
+
+HEADERS += \
+    comchange.h \               # 串口热插拔监测
+    mousekeytest.h \            # 鼠标、键盘热插拔监测
+    porttest.h \
+    widget.h
+
+FORMS += \
+    mousekeytest.ui \
+    porttest.ui \
+    widget.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+contains(QT_ARCH, i386){        # 使用32位编译器
+DESTDIR = $$PWD/../bin          # 程序输出路径
+}else{
+message("64bit")                # 使用64位编译器
+DESTDIR = $$PWD/../bin64
+}
---
+++ DeviceManagement/comchange.cpp
@@ -0,0 +1,106 @@
+﻿#pragma execution_character_set("utf-8")
+#include "comchange.h"
+#include <QApplication>
+#include <QMutex>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <qdebug.h>
+
+#ifdef Q_OS_WIN
+#include <windows.h>
+#include "dbt.h"
+#pragma comment(lib, "user32.lib")
+#endif
+
+ComChange* ComChange::m_comChange = nullptr;
+ComChange *ComChange::getInstance()
+{
+    if(m_comChange == nullptr)
+    {
+        static QMutex mutex;         //实例互斥锁。
+        QMutexLocker locker(&mutex); //加互斥锁。
+        if(m_comChange == nullptr)
+        {
+            m_comChange = new ComChange();
+        }
+    }
+    return m_comChange;
+
+}
+
+/**
+ * @brief   获取系统中所有可用的串口名
+ * @return
+ */
+QStringList ComChange::getAvailablePort()
+{
+    QStringList strName;
+    foreach(const QSerialPortInfo& info, QSerialPortInfo::availablePorts())
+    {
+        QSerialPort port(info);
+        if(port.open(QIODevice::ReadWrite))
+        {
+            strName << info.portName();
+            port.close();
+        }
+    }
+    return strName;
+}
+
+/**
+ * @brief       设置窗口句柄用于过滤事件
+ * @param hwnd
+ */
+void ComChange::setHWND(HWND hwnd)
+{
+    this->m_hwnd = hwnd;
+}
+
+ComChange::ComChange(QObject *parent) : QObject(parent)
+{
+    qApp->installNativeEventFilter(this);        // 安装事件过滤器
+
+}
+
+bool ComChange::nativeEventFilter(const QByteArray &eventType, void *message, long *result)
+{
+#ifdef Q_OS_WIN
+    if(eventType == "windows_generic_MSG")           // 处理windows消息
+    {
+        MSG* msg = reinterpret_cast<MSG*>(message);
+
+        if(msg->message == WM_DEVICECHANGE               // 通知应用程序设备或计算机的硬件配置发生更改。
+          && msg->hwnd == this->m_hwnd)                  // 过滤事件
+        {
+            PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)msg->lParam;
+            switch (msg->wParam)
+            {
+            case DBT_DEVICEARRIVAL:             // 插入
+            {
+                if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT)           // 设备类型为串口
+                {
+                    PDEV_BROADCAST_PORT lpdbv = (PDEV_BROADCAST_PORT)lpdb;
+                    QString strName = QString::fromWCharArray(lpdbv->dbcp_name);  //插入的串口名
+                    emit comStatus(strName, true);
+                }
+                break;
+            }
+            case DBT_DEVICEREMOVECOMPLETE:      // 拔出
+            {
+                if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT)           // 设备类型为串口
+                {
+                    PDEV_BROADCAST_PORT lpdbv = (PDEV_BROADCAST_PORT)lpdb;
+                    QString strName = QString::fromWCharArray(lpdbv->dbcp_name);  //拔出的串口名
+                    emit comStatus(strName, false);
+                }
+                break;
+            }
+            default:
+                break;
+            }
+        }
+    }
+#endif
+
+    return false;
+}
