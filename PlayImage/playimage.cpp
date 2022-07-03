283f71101fd2f11e9c944ad388b7ca8cf6620022
mhf <1603291350@qq.com>
2022-07-03 15:15:23 +0800

    新功能：增加图片缩放功能

--- PlayImage/playimage.cpp
+++ PlayImage/playimage.cpp
@@ -11,14 +11,13 @@ PlayImage::PlayImage(QWidget *parent) : QWidget(parent)
  * @brief        传入Qimage图片显示
  * @param image
  */
-void PlayImage::updateImage(const QImage &image)
+void PlayImage::updateImage(const QImage& image)
 {
-    m_pixmap = QPixmap::fromImage(image);
-    update();
+    updatePixmap(QPixmap::fromImage(image));
 }
 
 /**
- * @brief         传入QPixmap图片显示
+ * @brief        传入QPixmap图片
  * @param pixmap
  */
 void PlayImage::updatePixmap(const QPixmap &pixmap)
@@ -33,7 +32,19 @@ void PlayImage::updatePixmap(const QPixmap &pixmap)
  */
 void PlayImage::paintEvent(QPaintEvent *event)
 {
-    QPainter painter(this);
-    painter.drawPixmap(this->rect(), m_pixmap);
+    if(!m_pixmap.isNull())
+    {
+        QPainter painter(this);
+#if 0
+        // 经过粗略测试，QImage先缩放后转为QPixmap的方式在图像比较小时耗时少，图片越大耗时远大
+        QPixmap pixmap = QPixmap::fromImage(m_image.scaled(this->size(), Qt::KeepAspectRatio));
+        // 先将QImage转换为QPixmap再进行缩放则耗时比较少，并且稳定，不会因为缩放图片大小而产生太大影响
+        QPixmap pixmap1 = QPixmap::fromImage(m_image).scaled(this->size(), Qt::KeepAspectRatio);
+#endif
+        QPixmap pixmap = m_pixmap.scaled(this->size(), Qt::KeepAspectRatio);
+        int x = (this->width() - pixmap.width()) / 2;
+        int y = (this->height() - pixmap.height()) / 2;
+        painter.drawPixmap(x, y, pixmap);
+    }
     QWidget::paintEvent(event);
 }
