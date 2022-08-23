5a468583cc4c97a48130964412a0c4974c54246d
mhf <1603291350@qq.com>
2022-08-23 17:42:09 +0800

    新功能：完成TransformDemo旋转功能
        1、实现旋转功能；
        2、解决重置时控制未复位bug。

--- PaintingDemo/TransformDemo/widget.cpp
+++ PaintingDemo/TransformDemo/widget.cpp
@@ -30,14 +30,24 @@ bool Widget::eventFilter(QObject *watched, QEvent *event)
 {
     if(watched == ui->renderArea && event->type() == QEvent::Resize)
     {
-        ui->horizontalSlider_origin->setMaximum(ui->renderArea->width());
-        ui->verticalSlider_origin->setMaximum(ui->renderArea->height());
-
-        ui->horizontalSlider_origin->setValue(0);
-        ui->verticalSlider_origin->setValue(ui->renderArea->height());
+        reset();
     }
 
     return  QWidget::eventFilter(watched, event);
+}
+
+/**
+ * @brief  重置参数
+ */
+void Widget::reset()
+{
+    ui->horizontalSlider_origin->setMaximum(ui->renderArea->width());
+    ui->verticalSlider_origin->setMaximum(ui->renderArea->height());
+
+    ui->horizontalSlider_origin->setValue(0);
+    ui->verticalSlider_origin->setValue(ui->renderArea->height());
+
+    ui->dial->setValue(270);
 }
 
 /**
@@ -54,6 +64,7 @@ void Widget::on_but_original_clicked()
 void Widget::on_but_reset_clicked()
 {
     m_transform.reset();
+    reset();
     ui->renderArea->setTransform(m_transform);
 }
 
@@ -72,8 +83,11 @@ void Widget::on_but_transform_clicked()
 void Widget::on_horizontalSlider_origin_valueChanged(int value)
 {
     static int pastValue = 0;
-    m_transform.translate(value - pastValue,  0);           // 由于QTransform设置时累加的，所以这里需要通过pastValue计算差值
+    pastValue = m_transform.isIdentity() ? 0 : pastValue;  // 重置时对pastValue进行初始化
+    m_transform.translate(-pastValue, 0);                  // 由于QTransform设置时累加的，所以这里需要通过pastValue 减去之前的值
+
     pastValue = value;
+    m_transform.translate(value,  0);
     ui->renderArea->setTransform(m_transform);
 }
 
@@ -84,7 +98,25 @@ void Widget::on_horizontalSlider_origin_valueChanged(int value)
 void Widget::on_verticalSlider_origin_valueChanged(int value)
 {
     static int pastValue = 0;
-    m_transform.translate(0, ui->verticalSlider_origin->maximum() - value - pastValue);
+    pastValue = m_transform.isIdentity() ? 0 : pastValue;          // 重置时对pastValue进行初始化
+    m_transform.translate(0, -pastValue);                          // 由于QTransform设置时累加的，所以这里需要通过pastValue 减去之前的值
+
     pastValue = ui->verticalSlider_origin->maximum() - value;
+    m_transform.translate(0, pastValue);
+    ui->renderArea->setTransform(m_transform);
+}
+
+/**
+ * @brief          设置旋转功能
+ * @param value
+ */
+void Widget::on_dial_valueChanged(int value)
+{
+    static int pastValue = 0;
+    pastValue = m_transform.isIdentity() ? 0 : pastValue;
+    m_transform.rotate(-pastValue);             // 由于QTransform中rotate是累计的，所以在设置之前这里进行重置
+    pastValue = ((value + 90) % 360);
+
+    m_transform.rotate(pastValue);      // 由于QDial默认0为竖直向下方向，QPainter默认0度为向右，所以需要+90度
     ui->renderArea->setTransform(m_transform);
 }
--- PaintingDemo/TransformDemo/widget.h
+++ PaintingDemo/TransformDemo/widget.h
@@ -18,6 +18,9 @@ public:
 protected:
     bool eventFilter(QObject *watched, QEvent *event) override;
 
+private:
+    void reset();
+
 private slots:
 
     void on_but_reset_clicked();
@@ -29,6 +32,8 @@ private slots:
     void on_horizontalSlider_origin_valueChanged(int value);
 
     void on_verticalSlider_origin_valueChanged(int value);
+
+    void on_dial_valueChanged(int value);
 
 private:
     Ui::Widget *ui;
