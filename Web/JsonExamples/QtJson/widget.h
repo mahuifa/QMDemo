bbc6c745e0dea786578b0f647e112a39c887d9f6
mhf <1603291350@qq.com>
2022-12-13 17:07:52 +0800

    feat：使用内置json库读写json示例
        1、实现生成json数据，插入json数组、json对象以及各种json支持的数据类型；
        2、将json数据保存到json文件中；
        3、从json文件中读取json数据，并使用【递归】的方式解析json数据。

--- Web/JsonExamples/QtJson/widget.h
+++ Web/JsonExamples/QtJson/widget.h
@@ -3,6 +3,12 @@
 
 #include <QWidget>
 
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QJsonValue>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
@@ -16,6 +22,18 @@ public:
     ~Widget();
 
 private:
+    void jsonParse(const QJsonValue& value);
+
+private slots:
+    void on_but_create_clicked();
+    
+    void on_but_parse_clicked();
+    
+    void on_but_save_clicked();
+
+private:
     Ui::Widget *ui;
+
+    QJsonDocument m_doc;    // 创建一个用于读写json的对象
 };
 #endif // WIDGET_H
