ffffd0e52f28d4d309daab0cb10bdb36feec8841
mhf <1603291350@qq.com>
2023-01-08 17:45:19 +0800

    feat：完成Qt使用路径功能Demo

--- Experience/Experience.pro
+++ Experience/Experience.pro
@@ -1,3 +1,3 @@
 TEMPLATE = subdirs
 
-SUBDIRS += UsePath       # 演示了Windows下、Linux下Qt使用各种相对路径、绝对路径、系统路径、快捷方式、软硬链接的方式和注意事项
+SUBDIRS += UsePath       # Qt使用路径的方式和注意事项
--- Experience/UsePath/widget.cpp
+++ Experience/UsePath/widget.cpp
@@ -21,7 +21,8 @@ Widget::Widget(QWidget *parent)
     paths << QPair<QString, QString>("QDir::homePath()", QDir::homePath());          // 返回用户主目录的绝对路径
     paths << QPair<QString, QString>("QDir::rootPath()", QDir::rootPath());          // 返回根目录的绝对路径
 
-    QDir dir("./test");   // 指定一个路径
+    // Windows下创建一个文件夹快捷方式或linux下创建一个文件夹链接，将快捷方式路径写入下列QDir
+    QDir dir("./桌面文件夹 - 快捷方式");   // 指定一个路径
     paths << QPair<QString, QString>("QDir::path()", dir.path());                    // 返回输入路径，返回的路径可以是绝对路径或相对路径
     paths << QPair<QString, QString>("QDir::absolutePath()", dir.absolutePath());    // 返回绝对路径(不区分是不是快捷方式/linux链接 ln)
     // 如果是真实路径，效果和absolutePath()相同，如果是windows快捷方式则返回空，如果是linux软硬链接则返回路径地址的实际路径
@@ -29,8 +30,8 @@ Widget::Widget(QWidget *parent)
     paths << QPair<QString, QString>("QCoreApplication::applicationDirPath()", QCoreApplication::applicationDirPath());  // 返回包含应用程序可执行文件的目录
     paths << QPair<QString, QString>("qApp->applicationDirPath()", qApp->applicationFilePath());      // 返回应用程序可执行文件的文件路径
 
-    QFileInfo info("./1.txt");
-
+    // Windows下创建一个文件快捷方式或linux下创建一个文件链接，将快捷方式路径写入下列QDir
+    QFileInfo info("./桌面文件夹 - 快捷方式/1.xlsx");
     // 如果是真实路径，效果和absolutePath()相同，如果是windows快捷方式则返回空，如果是linux软硬链接则返回路径地址的实际路径
     paths << QPair<QString, QString>("QFileInfo::canonicalPath()", info.canonicalPath());
     paths << QPair<QString, QString>("QFileInfo::canonicalFilePath()", info.canonicalFilePath());
