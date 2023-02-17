a356e4a909d2c9e068f47041a69bf7133bc82700
mhf <1603291350@qq.com>
2023-02-17 11:59:00 +0800

    feat：增加Qt使用线程池QThreadPool示例
        1、QT使用QThreadPool、QRunnable的简单示例程序；
        2、演示了使用全局线程池和局部线程池；
        3、演示了在添加任务后修改最大线程个人导致的内存泄漏问题和解决办法。

---
+++ ConcurrentExamples/UseQThreadPool/UseQThreadPool.pro
@@ -0,0 +1,53 @@
+#---------------------------------------------------------------------------------------
+# @功能：      Qt使用线程池QThreadPool示例
+# @编译器：     Desktop Qt 5.12.5 MSVC2017 64bit（也支持其它编译器）
+# @Qt IDE：    D:/Qt/Qt5.12.5/Tools/QtCreator/share/qtcreator
+#
+# @开发者     mhf
+# @邮箱       1603291350@qq.com
+# @时间       2023-02-17 11:55:27
+# @备注       1、QT使用QThreadPool、QRunnable的简单示例程序；
+#            2、演示了使用全局线程池和局部线程池；
+#            3、演示了在添加任务后修改最大线程个人导致的内存泄漏问题和解决办法。
+#---------------------------------------------------------------------------------------
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+CONFIG += c++11
+DEFINES += QT_DEPRECATED_WARNINGS
+
+SOURCES += \
+    main.cpp \
+    widget.cpp
+
+HEADERS += \
+    widget.h
+
+FORMS += \
+    widget.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+
+#  定义程序版本号
+VERSION = 1.0.0
+DEFINES += APP_VERSION=\\\"$$VERSION\\\"
+
+contains(QT_ARCH, i386){        # 使用32位编译器
+DESTDIR = $$PWD/../bin          # 程序输出路径
+}else{
+DESTDIR = $$PWD/../bin64        # 使用64位编译器
+}
+
+# msvc >= 2017  编译器使用utf-8编码
+msvc {
+    greaterThan(QMAKE_MSC_VER, 1900){       # msvc编译器版本大于2015
+        QMAKE_CFLAGS += /utf-8
+        QMAKE_CXXFLAGS += /utf-8
+    }else{
+        message(msvc2015及以下版本在代码中使用【pragma execution_character_set("utf-8")】指定编码)
+    }
+}
---
+++ ConcurrentExamples/UseQThreadPool/widget.cpp
@@ -0,0 +1,133 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QRunnable>
+#include <qthread.h>
+#include <QDebug>
+#include <QThreadPool>
+#include <QMutex>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    this->setWindowTitle(QString("Qt使用线程池QThreadPool--V%1").arg(APP_VERSION));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+static int g_deleteNum = 0;
+static QMutex g_mutex;
+// 用于在线程池中执行异步任务的类
+class Task : public QRunnable
+{
+public:
+    ~Task() override
+    {
+        QMutexLocker locker(&g_mutex);
+        qDebug() << "销毁任务对象，释放个数：" << ++g_deleteNum;
+    }
+    void run() override
+    {
+        QThread::sleep(1);
+        qDebug() << "执行任务：" << QThread::currentThreadId() ;
+    }
+};
+
+/**
+ * @brief 使用全局线程池
+ */
+void Widget::on_pushButton_clicked()
+{
+    g_deleteNum = 0;
+    QThreadPool* pool = QThreadPool::globalInstance();  // 获取全局线程池
+    int taskCount = ui->spinBox->value();
+    int threadCount = ui->spinBox_2->value();
+    if(threadCount > 0)   // 如果为0则使用默认值
+    {
+        pool->setMaxThreadCount(threadCount);  // 设置最大线程个数，但一般不需要设置
+    }
+    for(int i = 0; i < taskCount; i++)
+    {
+        pool->start(new Task());    // 向线程池中添加任务
+    }
+
+    qDebug() << "最大线程数：" << pool->maxThreadCount();
+//    pool->clear();          // 清除所有当前排队但未开始运行的任务
+//    pool->waitForDone();    // 等待线程中所有任务执行完成（会阻塞界面）
+    qDebug() << "任务过期时间：" << pool->expiryTimeout();
+    qDebug() << "线程池工作线程的堆栈大小：" << pool->stackSize();   // 默认为0
+}
+
+/**
+ * @brief 使用局部线程池（内存泄漏更严重）
+ *        当设置的最大线程数大于默认线程个数时将会存在内存泄漏
+ */
+void Widget::on_pushButton_2_clicked()
+{
+    g_deleteNum = 0;
+    QThreadPool pool;
+    int taskCount = ui->spinBox->value();
+    int threadCount = ui->spinBox_2->value();
+    if(threadCount > 0)   // 如果为0则使用默认值
+    {
+        pool.setMaxThreadCount(threadCount);  // 设置最大线程个数，但一般不需要设置
+    }
+    for(int i = 0; i < taskCount; i++)
+    {
+        pool.start(new Task());    // 向线程池中添加任务
+    }
+
+    qDebug() << "最大线程数：" << pool.maxThreadCount();
+//    pool.clear();          // 清除所有当前排队但未开始运行的任务
+//    pool.waitForDone();    // 等待线程中所有任务执行完成（会阻塞界面）
+    qDebug() << "任务过期时间：" << pool.expiryTimeout();
+    qDebug() << "线程池工作线程的堆栈大小：" << pool.stackSize();
+}
+
+/**
+ * @brief 可能存在内存泄漏
+ */
+void Widget::on_pushButton_3_clicked()
+{
+    g_deleteNum = 0;
+    QThreadPool* pool = QThreadPool::globalInstance();  // 获取全局线程池
+
+    for(int i = 0; i < 100; i++)
+    {
+        pool->start(new Task());    // 向线程池中添加任务
+    }
+
+
+    // 如果在已经添加任务后修改最大线程个数，需要先调用 QThreadPool::clear() 函数清空任务队列，
+    // 否则可能会导致已经加入任务队列但尚未执行的任务被丢弃，从而引发内存泄漏。
+//    pool->clear();
+    pool->setMaxThreadCount(20);  // 设置最大线程个数，但一般不需要设置
+}
+
+/**
+ * @brief 存在内存泄漏
+ *        局部QThreadPool会默认调用waitForDone()，防止函数退出时释放局部对象
+ */
+void Widget::on_pushButton_4_clicked()
+{
+    g_deleteNum = 0;
+    QThreadPool pool;
+
+    for(int i = 0; i < 100; i++)
+    {
+        pool.start(new Task());    // 向线程池中添加任务
+    }
+
+
+    // 如果在已经添加任务后修改最大线程个数，需要先调用 QThreadPool::clear() 函数清空任务队列，
+    // 否则可能会导致已经加入任务队列但尚未执行的任务被丢弃，从而引发内存泄漏。
+//    pool.clear();
+    pool.setMaxThreadCount(20);  // 设置最大线程个数，但一般不需要设置
+}
