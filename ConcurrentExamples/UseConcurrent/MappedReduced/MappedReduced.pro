5143a48d3d5663de2fb7b259da13c810467d1e72
mhf <1603291350@qq.com>
2023-03-12 11:31:03 +0800

    feat： 增加QtConcurrent::mappedReduced使用示例

---
+++ ConcurrentExamples/UseConcurrent/MappedReduced/MappedReduced.pro
@@ -0,0 +1,38 @@
+#---------------------------------------------------------------------------------------
+# @功能：       QtConcurrent::mappedReduced使用示例
+# @编译器：     Desktop Qt 5.12.5 MSVC2017 64bit（也支持其它编译器）
+# @Qt IDE：    D:/Qt/Qt5.12.5/Tools/QtCreator/share/qtcreator
+#
+# @开发者     mhf
+# @邮箱       1603291350@qq.com
+# @时间       2023-03-12 09:21:10
+# @备注       将参数1数组传入，在参数2的函数中多线程并行计算，然后将参数2的函数的返回值传入参数3的函数中，在参数3的函数中单线程计算
+#---------------------------------------------------------------------------------------
+QT -= gui
+QT += concurrent
+
+CONFIG += c++11 console
+CONFIG -= app_bundle
+DEFINES += QT_DEPRECATED_WARNINGS
+
+SOURCES += main.cpp
+
+#  定义程序版本号
+VERSION = 1.0.0
+DEFINES += APP_VERSION=\\\"$$VERSION\\\"
+
+contains(QT_ARCH, i386){        # 使用32位编译器
+DESTDIR = $$PWD/../bin          # 程序输出路径
+}else{
+DESTDIR = $$PWD/../bin64        # 使用64位编译器
+}
+
+# msvc >= 2017  编译器使用utf-8编码
+msvc {
+    greaterThan(QMAKE_MSC_VER, 1900){       # msvc编译器版本大于2015
+        QMAKE_CFLAGS += /utf-8
+        QMAKE_CXXFLAGS += /utf-8
+    }else{
+        message(msvc2015及以下版本在代码中使用【pragma execution_character_set("utf-8")】指定编码)
+    }
+}
---
+++ ConcurrentExamples/UseConcurrent/MappedReduced/main.cpp
@@ -0,0 +1,66 @@
+#include <QCoreApplication>
+#include <qtconcurrentmap.h>
+#include <qthread.h>
+#include <QDebug>
+
+using namespace QtConcurrent;
+
+/**
+ * @brief          sum()函数每次返回结果，reduce函数将被调用一次，并且应该将中间值合并到结果变量中。（单线程执行）
+ *                 QtConcurrent:：mappedReduced（）保证一次只有一个线程会调用reduce，因此不需要使用互斥锁来锁定结果变量。
+ *                 QtConcurrent:：ReduceOptions枚举提供了一种控制减少顺序的方法。
+ *                 如果使用QtConcurrent:：UnorderReduce（默认值），则顺序未定义，而QtConcCurrent:：OrderedReduce则确保按原始序列的顺序进行缩减。
+ * @param arr      sum()函数的返回值
+ * @param value    result()函数的返回值，将当前函数中的结果通过这个值返回出去
+ */
+void result1(quint64& arr, quint64 value)
+{
+    qDebug() << QString("同步计算，线程：")<< QThread::currentThreadId();
+    arr += value;
+    QThread::msleep(500);
+}
+
+void result2(QList<quint64>& arr, quint64 value)
+{
+    qDebug() << QString("同步计算，线程：")<< QThread::currentThreadId();
+    arr.push_back(value + 10000);
+    QThread::msleep(500);
+}
+
+/**
+ * @brief      在线程中异步计算数据，并返回计算的结果（并行）
+ * @param max
+ * @return
+ */
+quint64 sum(quint64 max)
+{
+    qDebug() << QString("开始计算从0~%1的和，线程：").arg(max) << QThread::currentThreadId();
+    quint64 sum = 0;
+    for(quint64 i = 0; i < max; i++)
+    {
+        sum += i;
+    }
+
+    qDebug() << QString("完成计算从0~%1的和为：%1，线程：").arg(sum) << QThread::currentThreadId();
+    return sum;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QList<quint64> arr = {10, 15, 20, 25};
+#if 1
+    quint64 collage = mappedReduced(arr, sum, result1);
+    qDebug() << "计算完成" << collage;
+#else
+    QList<quint64> collage = mappedReduced(arr, sum, result2);    // 返回值类型和result()函数第一个参数相同
+    qDebug() << "计算完成" ;
+    for(auto v : collage)
+    {
+        qDebug() << v;
+    }
+#endif
+
+    return a.exec();
+}
