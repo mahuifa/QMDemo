665ae475e1e88383bf799f993596a8896a4674b6
mhf <1603291350@qq.com>
2024-04-10 22:30:23 +0800

    docs：修改MapDownload.md说明文件

--- MapExamples/MapExamples.md
+++ MapExamples/MapExamples.md
@@ -25,11 +25,62 @@
 
 ## 3、构建说明
 
+* 由于瓦片地图下载使用到`QNetworkAccessManager`类，而这个类需要openssl支持；
 
+* qt默认安装是不带openssl的，需要自己配置openssl环境；
+
+* **安装openssl**
+
+  * 使用下列代码打印qt版本支持的ssl版本；
+
+    ```c++
+    qDebug() << "输出当前QT支持的openSSL版本: " << QSslSocket::sslLibraryBuildVersionString();
+    qDebug() << "OpenSSL支持情况: " <<QSslSocket::supportsSsl();
+    qDebug() << "OpenSSL运行时SSL库版本: " << QSslSocket::sslLibraryBuildVersionString();
+    ```
+
+  * windows可以[下载](https://slproweb.com/products/Win32OpenSSL.html)对应版本的openssl，然后进行安装；
+
+  * linux可以通过命令行安装，也可以下载源码自己编译。
+
+  * openssl的github[仓库](https://github.com/openssl/openssl)
+
+  * openssl[官网](https://www.openssl.org/)
+
+  * 安装后将openssl/bin文件夹下的libcrypto-1_1-x64.dll、libssl-1_1-x64.dll两个动态库拷贝到qt的编译器路径下，注意区分32和64位
+
+    * D:\Qt\Qt5.14.2\5.14.2\msvc2017_64\bin
+    * D:\Qt\Qt5.14.2\5.14.2\mingw73_64\bin
+
+  
 
 ## 4、实现效果
 
 ### 1.1 MapDownload
 
-> 
+> 1. 支持单线程、多线程下载瓦片地图。
+> 2. 支持下载多样式arcGis瓦片地图；
+> 3. 支持下载多样式高德瓦片地图；
+> 4. 支持Bing地图下载。
+
+![image-20240510221601118](./MapExamples.assets/image-20240510221601118.png)
+
+### 1.2 MapView
+
+> 1. 支持多线程加载显示本地离线瓦片地图(墨卡托投影)；
+> 2. 瓦片切片规则以左上角为原点（谷歌、高德、ArcGis等），不支持百度瓦片规则
+> 3. 支持显示瓦片网格、编号信息；
+> 4. 以QGraphicsView坐标系绘制瓦片地图。
+
+![image-20240510221755393](./MapExamples.assets/image-20240510221755393.png)
+
+### 1.2 MapView2
+
+> 1. 加载显示本地离线瓦片；
+> 2. 使用**绝对像素坐标**方式显示瓦片；
+> 3. 支持以**鼠标为原点缩放**瓦片层级；
+> 4. 支持在线程池中快速加载本地瓦片；
+> 5. 为了示例更加简单，复杂功能没在这个示例实现，会加载当前层级所有瓦片。
+
+![image-20240510222139643](./MapExamples.assets/image-20240510222139643.png)
 
--- QMNetwork/SimpleNetWidget/simpleudpgroup.cpp
+++ QMNetwork/SimpleNetWidget/simpleudpgroup.cpp
@@ -2,17 +2,19 @@
 #include "ui_simpleudpgroup.h"
 
 #include <qnetworkdatagram.h>
+#include <QNetworkInterface>
 
-SimpleUdpGroup::SimpleUdpGroup(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::SimpleUdpGroup)
+SimpleUdpGroup::SimpleUdpGroup(QWidget* parent)
+    : QWidget(parent)
+    , ui(new Ui::SimpleUdpGroup)
 {
     ui->setupUi(this);
 
     this->setWindowTitle("简易版UDP组播通信Demo");
 
     m_udpSocket = new QUdpSocket(this);
-    connect(m_udpSocket, &QUdpSocket::readyRead, this, &SimpleUdpGroup::on_readyRead);     // 当有可读数据时发出readyRead信号
+    connect(m_udpSocket, &QUdpSocket::readyRead, this,
+            &SimpleUdpGroup::on_readyRead);   // 当有可读数据时发出readyRead信号
 }
 
 SimpleUdpGroup::~SimpleUdpGroup()
@@ -25,26 +27,31 @@ SimpleUdpGroup::~SimpleUdpGroup()
  */
 void SimpleUdpGroup::on_readyRead()
 {
-    QNetworkDatagram datagram = m_udpSocket->receiveDatagram();  // 读取数据，这里需要添加qnetworkdatagram.h头文件
-    ui->text_recv->append(datagram.data());                      // 显示读取到的数据
+    QNetworkDatagram datagram = m_udpSocket->receiveDatagram();   // 读取数据，这里需要添加qnetworkdatagram.h头文件
+    ui->text_recv->append(datagram.data());                       // 显示读取到的数据
 }
-
 
 /**
  * @brief 开始绑定绑定IP端口，用于接收数据，并加入组播组
  */
 void SimpleUdpGroup::on_but_connect_clicked()
 {
-    if(m_udpSocket->state() != QAbstractSocket::BoundState)                                        // 判断是否绑定绑定端口
+    if (m_udpSocket->state() != QAbstractSocket::BoundState)   // 判断是否绑定绑定端口
     {
-        bool ret = m_udpSocket->bind(QHostAddress::AnyIPv4,                                        // 使用组播时这里必须要指定AnyIPv4，不指定将会默认Any，加入组播会失败
-                                     ui->spin_groupPort->value(),                                  // 绑定绑定接收数据的端口，一般用组播端口
-                                     QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);     // ShareAddress允许一台电脑上多个程序绑定同一个端口，便于测试，但是在windows下需要加上ReuseAddressHint才有效
-        if(ret)
+        bool ret = m_udpSocket->bind(
+            QHostAddress::AnyIPv4,   // 使用组播时这里必须要指定AnyIPv4，不指定将会默认Any，加入组播会失败
+            ui->spin_groupPort->value(),   // 绑定绑定接收数据的端口，一般用组播端口
+            QUdpSocket::ShareAddress
+                | QUdpSocket::ReuseAddressHint);   // ShareAddress允许一台电脑上多个程序绑定同一个端口，便于测试，但是在windows下需要加上ReuseAddressHint才有效
+        if (ret)
         {
+            for (auto face : QNetworkInterface::allInterfaces())
+            {
+                qDebug() << face.allAddresses() << face.name();
+            }
             qInfo() << "绑定本地地址成功！";
-            ret = m_udpSocket->joinMulticastGroup(QHostAddress(ui->line_groupAddress->text()));           // 加入组播组
-            if(ret)
+            ret = m_udpSocket->joinMulticastGroup(QHostAddress(ui->line_groupAddress->text()));   // 加入组播组
+            if (ret)
             {
                 qInfo() << "加入组播组成功！";
                 ui->but_connect->setText("关闭");
@@ -62,7 +69,7 @@ void SimpleUdpGroup::on_but_connect_clicked()
     else
     {
         bool ret = m_udpSocket->leaveMulticastGroup(QHostAddress(ui->line_groupAddress->text()));
-        if(ret)
+        if (ret)
         {
             qInfo() << "移除组播组成功！";
             m_udpSocket->abort();
@@ -75,14 +82,13 @@ void SimpleUdpGroup::on_but_connect_clicked()
     }
 }
 
-
 /**
  * @brief 发送数据，由于已经绑定了目标IP和目标端口号，在发送数据时就不用指定IP、端口了
  */
 void SimpleUdpGroup::on_but_send_clicked()
 {
     QString str = ui->text_send->toPlainText();
-    qint64 len = m_udpSocket->writeDatagram(str.toUtf8(), QHostAddress(ui->line_groupAddress->text()), ui->spin_groupPort->value());
+    qint64 len = m_udpSocket->writeDatagram(str.toUtf8(), QHostAddress(ui->line_groupAddress->text()),
+                                            ui->spin_groupPort->value());
     qInfo() << QString("发送数据长度：%1").arg(len);
 }
-
